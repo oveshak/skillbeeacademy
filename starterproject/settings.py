31a40c2e5b34db400ed91bc632b63f43fdfe7d5c
oveshak <oveshaktoton9999@gmail.com>
2026-02-02 22:03:52 +0600

    cicd

--- starterproject/settings.py
+++ starterproject/settings.py
@@ -29,6 +29,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://skillbeeacademy.onrender.com',
+    # Add other trusted origins if needed
+]
 
 # Application definition
 
